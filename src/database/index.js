03f006e661b1b8e323808f15af6a106856f0151c
r0bfp <robfpinheiro@gmail.com>
2022-01-06 23:44:50 -0300

    feat: implementation of course, user and modality

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,20 @@
 const Sequelize = require('sequelize');
 
 const config = require('../config/database');
+
 const Course = require('../models/Course');
+const User = require('../models/User');
+const Modality = require('../models/Modality');
 
 const connection = new Sequelize(config);
 
 Course.init(connection);
+User.init(connection);
+Modality.init(connection);
+
+Course.associate(connection.models)
+User.associate(connection.models)
+Modality.associate(connection.models)
 
 
 module.exports = connection;
