d6dc193e7097a11bedeab91ac15f88765b72072e
r0bfp <robfpinheiro@gmail.com>
2022-04-15 20:08:46 -0300

    fix: solve cors issue

--- src/config/server.js
+++ src/config/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const routes = require('../routes.js');
 require('../database/index.js');
@@ -7,6 +8,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 module.exports = { app, PORT };
