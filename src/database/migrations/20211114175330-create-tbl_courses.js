93e6eacf6c8569a7905029a6195a985aae00c04b
r0bfp <robfpinheiro@gmail.com>
2022-04-15 20:09:53 -0300

    feat: add field type

--- src/database/migrations/20211114175330-create-tbl_courses.js
+++ src/database/migrations/20211114175330-create-tbl_courses.js
@@ -13,6 +13,10 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
+      type: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
       duration: {
         type: Sequelize.INTEGER,
         allowNull: false,
